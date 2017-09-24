b07ba8f4ce810b3f74b532d6cabb99acadac948f
masato121 <oka0121masa@gmail.com>
2017-09-24 12:42:19 +0900

    タイトルシーン（仮）
    とりあえず作ってみました。画像は全部フリー素材。
    [clear]ボタンでクリア済みの、[noclear]ボタンで未クリア時の演出が見ることが出来ます。あくまで（仮）なので改善点などありましたらどんどんあげてってください。

---
+++ TsubakuroProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public bool ClearFlag;
+	public GameObject flyingtsubame;
+	public GameObject tsubame;
+	public GameObject MiniGame;
+
+	// Use this for initialization
+	void Start () {	
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ClearFlag == true) {
+			flyingtsubame.active = true;
+			MiniGame.active = true;
+			tsubame.active = false;
+		} else {
+			flyingtsubame.active = false;
+			MiniGame.active = false;
+			tsubame.active = true;
+		}
+	}
+	public void PushClearButton (){
+		ClearFlag = true;
+	}
+	public void PushButtonNoclear(){
+		ClearFlag = false;
+	}
+
+}
